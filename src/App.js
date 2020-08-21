ad1c2a82623de5c3dba4f72741af9da24e02df64
nadiaGincoff <nadia.gincoff@gmail.com>
2020-08-21 15:00:05 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
+import AboutUs from "./components/AboutUs";
+import MedicalAttention from "./components/MedicalAttention";
+import SupportGroup from "./components/SupportGroup";
+import Content from "./components/Content";
+import Events from "./components/SupportGroup";
+import Contact from "./components/Contact";
 
-function App() {
-  return ( <
-    div className = "App" >
-    hola hola < /div>
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path='/contacto'>
+          <Contact />
+        </Route>
+        <Route path='/eventos'>
+          <Events />
+        </Route>
+        <Route path='/contenido'>
+          <Content />
+        </Route>
+        <Route path='/grupos-de-ayuda'>
+          <SupportGroup />
+        </Route>
+        <Route path='/atencion-medica'>
+          <MedicalAttention />
+        </Route>
+        <Route path='/quienes-somos'>
+          <AboutUs />
+        </Route>
+        <Route path='/'>
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
-}
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 export default App;
