678aae4a48fe755256a6e7c40f83dde4e1007e2b
GenesisAstete <genesis.astete.a@gmail.com>
2020-08-22 03:09:10 -0400

    tabla reserva especialistas

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,32 +3,6 @@ import { Link } from "react-router-dom";
 import '../styles/Navigation.css';
 
 const Navigation = () => {
-<<<<<<< HEAD
-  return (
-    <nav className="navContainer">
-      <ul className='navBar'>
-        <Link className="navItem" to="/quienes-somos">
-          <li>Quienes somos</li>
-        </Link>
-        <Link className="navItem" to="/atencion-medica">
-          <li>Atencion medica</li>
-        </Link>
-        <Link className="navItem" to="/grupos-de-ayuda">
-          <li>Grupos de ayuda</li>
-        </Link>
-        <Link className="navItem" to="/contenido">
-          <li>Contenido</li>
-        </Link>
-        <Link className="navItem" to="/eventos">
-          <li>Eventos</li>
-        </Link>
-        <Link className="navItem" to="/contacto">
-          <li>Contacto</li>
-        </Link>
-      </ul>
-    </nav>
-  );
-=======
     return (
         <nav className="navContainer">
           <ul className='navBar'>
@@ -56,7 +30,6 @@ const Navigation = () => {
           </ul>
         </nav>
     );
->>>>>>> 2a809078423770d7a85cf5ee734ac734dc9da431
 }
 
 export default Navigation;
